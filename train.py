487d6ae0a104bd3605949f5e20d98bc883067efb
Aleenasuhail <47885182+Aleenasuhail@users.noreply.github.com>
2020-08-07 12:17:24 +0100

    Update train.py

--- train.py
+++ train.py
@@ -8,13 +8,13 @@ from torch.utils.data import Dataset, DataLoader
 
 from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNetwork
-
+#opening and loading json data
 with open('intents.json', 'r') as f:
     intents = json.load(f)
 
 all_words = []
 tags = []
-xy = []
+xy = [] #empty list that will later hold patterns and tags
 # loop through each sentence in intents patterns in intents
 for intent in intents['intents']:
     tag = intent['tag']
@@ -24,9 +24,9 @@ for intent in intents['intents']:
         # tokenize each word in the sentence
         w = tokenize(pattern)
         # add to our words list
-        all_words.extend(w)
+        all_words.extend(w) #xetents the array append will put array in array
         # add to xy pair
-        xy.append((w, tag))
+        xy.append((w, tag)) #pattern and the corresponding tag
 
 # stem and lower each word
 ignore_words = ['?', '.', '!','@']
@@ -38,34 +38,34 @@ tags = sorted(set(tags))
 
 
 # create training data
-X_train = []
-y_train = []
-for (pattern_sentence, tag) in xy:
+X_train = []  #bag of words
+y_train = [] #associated number with each tags
+for (pattern_sentence, tag) in xy: #unpacking the tuple xy.append((w, tag))
     # X: bag of words for each pattern_sentence
-    bag = bag_of_words(pattern_sentence, all_words)
-    X_train.append(bag)
+    bag = bag_of_words(pattern_sentence, all_words) #bag_of_words takes tokenized sentence and all_words as an arguement
+    X_train.append(bag) #empty list X_tain gets updated with bag
     # y: CrossEntropyLoss needs only labels
-    label = tags.index(tag)
+    label = tags.index(tag) #labels to the tag for eg greetings 0
     y_train.append(label)
 
-X_train = np.array(X_train)
-y_train = np.array(y_train)
+X_train = np.array(X_train) #converting to numpy array
+y_train = np.array(y_train) #converting to numpy array
 
 # Hyper-parameters
-num_epochs = 1000   #training loops
-batch_size = 8      #no. of samples
-learning_rate = 0.011  #updates weights
-input_size = len(X_train[0])
-hidden_size = 8
-output_size = len(tags)
+num_epochs = 300 #training loops
+batch_size = 8    #no. of samples
+learning_rate = 0.001  #updates weights
+input_size = len(X_train[0]) #size of input layer
+hidden_size = 6 #no. of hidden neurons
+output_size = len(tags) #size of output layer
 
-
+#create new class ChatDataset which inherits dataset
 class ChatDataset(Dataset):
 
-    def __init__(self):
-        self.n_samples = len(X_train)
-        self.x_data = X_train
-        self.y_data = y_train
+    def __init__(self): #implement init function which gets self
+        self.n_samples = len(X_train) #strores the length of X_train array
+        self.x_data = X_train #strores data from X_train array
+        self.y_data = y_train #strores data from Y_train array
 
     # support indexing such that dataset[i] can be used to get i-th sample
     def __getitem__(self, index):
@@ -76,39 +76,46 @@ class ChatDataset(Dataset):
         return self.n_samples
 
 dataset = ChatDataset()
+#creating data loader
 train_loader = DataLoader(dataset=dataset,
                           batch_size=batch_size,
                           shuffle=True,
                           num_workers=2)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') #check if gpu is available
 
-model = NeuralNetwork(input_size, hidden_size, output_size).to(device)
+model = NeuralNetwork(input_size, hidden_size, output_size).to(device) #pushing model to device
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+#optimizer is used to update the attributes such as weight
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) #calculates adaptive learning rate for parameters
 
 # Train the model
-for epoch in range(num_epochs):
-    for (words, labels) in train_loader:
-        words = words.to(device)
-        labels = labels.to(device)
+for epoch in range(num_epochs):    #epochs in sequence e.g 0,1.....50
+    for (words, labels) in train_loader: #unpack words and labels
+        words = words.to(device) #push to device
+        labels = labels.to(device) #push to device
 
         # Forward pass
-        outputs = model(words)
+        outputs = model(words) #words are passed through the nn
 
-        loss = criterion(outputs, labels)
+        loss = criterion(outputs, labels)  #calculates loss using predicted output and actaul labels
 
         # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        optimizer.zero_grad() #empty gradient
+        loss.backward()  #calculate backpropogation
+        optimizer.step() #updates parameters based on loss.backward()
+    if (epoch+1) % 100 == 0:
+        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
 
-    
 
+print(f'final loss: {loss.item():.4f}')
+
+
+#saving data
 data = {
-"model_state": model.state_dict(),
+"model_state": model.state_dict(), #create dictionary to save model state
 "input_size": input_size,
 "hidden_size": hidden_size,
 "output_size": output_size,
@@ -116,8 +123,7 @@ data = {
 "tags": tags
 }
 
-FILE = "data.pth"
-torch.save(data, FILE)
+FILE = "data.pth" #define file name pth for pytorch
+torch.save(data, FILE) #serialize it and save to pickeled file
 
 print(f'training complete. file saved to {FILE}')
-
